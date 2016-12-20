9ea87192e3274194261bce071e8846e51f71242e
yhua123 <you@example.com>
2016-12-20 20:57:03 +0800

    update 20161220

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker save -o borathonimage.tgz findserver:0.1 rsserver:0.1
+docker save -o borathonimage.tgz findserver:0.1 rsserver:0.1 uiserver:0.1
 tar -zcvf borathon.tgz docker-compose.yaml install.sh borathonimage.tgz
